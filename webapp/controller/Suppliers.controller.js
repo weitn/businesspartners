b48741d8b1fd71ded62cf6b8ec9b664885d66587
Wei Eng Tan <weitn@hotmail.com>
2023-10-12 03:10:58 +0000

    Chg target

--- webapp/controller/Suppliers.controller.js
+++ webapp/controller/Suppliers.controller.js
@@ -12,21 +12,23 @@ sap.ui.define([
 
             },
 
-            pressMe: function() {
+            navToProducts: function() {
                 var lv_InputValue = this.getView().byId("input0").getValue(); //Get value from input box
 
                 alert(`Welcome! ${lv_InputValue}`);
 
                 //Navigate to Products view
                 //this.byId("Suppliers").to(this.byId("Products"));
+                //this.getRouter().getTargets().display("Products", { fromTarget: "Suppliers" });
+                this.getOwnerComponent().getRouter().getTargets().display("Products", { fromTarget: "Suppliers" });
             },
 
             onDisplayNotFound : function () {
                 alert('In onDisplayNotFound function');
 
                 //display the "notFound" target without changing the hash
-                this.getRouter().getTargets().display("notFound", { fromTarget: "Suppliers" });
-                //this.getOwnerComponent().getRouter().getTargets().display("notFound", { fromTarget: "Suppliers" });
+                //this.getRouter().getTargets().display("notFound", { fromTarget: "Suppliers" });
+                this.getOwnerComponent().getRouter().getTargets().display("NotFound", { fromTarget: "Suppliers" });
             }
         });
     });
