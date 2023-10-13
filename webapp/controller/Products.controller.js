40365c4f5479ca5f4cc2dc2b919390c24efab829
Wei Eng Tan <weitn@hotmail.com>
2023-10-13 23:05:57 +0000

    Chg files

--- webapp/controller/Products.controller.js
+++ webapp/controller/Products.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, History) {
         "use strict";
 
         return Controller.extend("ns.businesspartners.controller.Products", {
@@ -16,6 +17,20 @@ sap.ui.define([
                 var lv_InputValue = this.getView().byId("input0").getValue();
 
                 alert(`Welcome! ${lv_InputValue}`);
+            },
+
+            onNavBack: function () {
+                alert('In onNavBack function in Product Controller');
+
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = this.getOwnerComponent().getRouter();
+                    oRouter.navTo("TargetSuppliers", {}, true);
+                }
             }
         });
     });
--- webapp/controller/Suppliers.controller.js
+++ webapp/controller/Suppliers.controller.js
@@ -13,14 +13,14 @@ sap.ui.define([
             },
 
             navToProducts: function() {
-                var lv_InputValue = this.getView().byId("input0").getValue(); //Get value from input box
+                //var lv_InputValue = this.getView().byId("input0").getValue(); //Get value from input box
 
-                alert(`Welcome! ${lv_InputValue}`);
+                //alert(`Welcome! ${lv_InputValue}`);
 
                 //Navigate to Products view
                 //this.byId("Suppliers").to(this.byId("Products"));
                 //this.getRouter().getTargets().display("Products", { fromTarget: "Suppliers" });
-                this.getOwnerComponent().getRouter().getTargets().display("Products", { fromTarget: "Suppliers" });
+                this.getOwnerComponent().getRouter().getTargets().display("Products", { fromTarget: "TargetSuppliers" });
             },
 
             onDisplayNotFound : function () {
@@ -28,7 +28,7 @@ sap.ui.define([
 
                 //display the "notFound" target without changing the hash
                 //this.getRouter().getTargets().display("notFound", { fromTarget: "Suppliers" });
-                this.getOwnerComponent().getRouter().getTargets().display("NotFound", { fromTarget: "Suppliers" });
+                this.getOwnerComponent().getRouter().getTargets().display("NotFound", { fromTarget: "TargetSuppliers" });
             }
         });
     });
